f605a622680834e74f192285902a64d7b8fcd8ce
ksenia_kap_1 <kseniakap096@gmail.com>
2026-04-07 16:58:08 +0300

    feat: изменение gulpfile.js и зависимостей для сборки проекта перед деплоем

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,41 @@
-
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
-const sass = require("gulp-sass")(require("sass"));
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
-
 const htmlmin = require("gulp-htmlmin");
-// const { exec } = require('child_process');
+const imagemin = require("gulp-imagemin");
+const mozjpeg = require("imagemin-mozjpeg");
+const pngquant = require("imagemin-pngquant");
 
-// Static server
+
 gulp.task("server", function () {
-    browserSync.init({
-        server: {
-            baseDir: "dist",
-        },
-    });
+    browserSync.init({ server: { baseDir: "dist" } });
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("styles", function () {
     return gulp
-        .src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(
-            rename({
-                prefix: "",
-                suffix: ".min",
-            })
-        )
-        // .pipe(
-        //     autoprefixer({
-        //         browsers: ["last 2 versions"],
-        //         cascade: false,
-        //     })
-        // )
-        .pipe(cleanCSS({ compatibility: "ie8" }))
+        .src("src/css/style.css")
+        .pipe(autoprefixer({ overrideBrowserslist: ["last 2 versions"], cascade: false }))
         .pipe(gulp.dest("dist/css"))
-        // .pipe(gulp.dest("src/css"))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(cleanCSS({
+            compatibility: '*',
+            level: 1,
+            rebase: false
+        }))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task("watch", function () {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", gulp.parallel("html"));
-    gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
-    // gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
-    gulp.watch("src/icons/**/*").on("add", gulp.parallel("icons"));
-    gulp.watch("src/img/**/*").on("add", gulp.parallel("images"));
-});
-
+// HTML
 gulp.task("html", function () {
     return gulp
         .src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("dist/"));
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("scripts", function () {
@@ -64,12 +45,6 @@ gulp.task("scripts", function () {
         .pipe(browserSync.stream());
 });
 
-// gulp.task("fonts", function () {
-//     return gulp
-//         .src("src/fonts/**/*")
-//         .pipe(gulp.dest("dist/fonts"))
-//         .pipe(browserSync.stream());
-// });
 
 gulp.task("icons", function () {
     return gulp
@@ -80,22 +55,31 @@ gulp.task("icons", function () {
 
 gulp.task("images", function () {
     return gulp
-        .src("src/img/**/*")
-        // .pipe(imagemin())//дз
+        .src("src/img/**/*.{png,jpg,jpeg,gif,svg}")
+        .pipe(
+            imagemin([
+                mozjpeg({ quality: 80, progressive: true }),
+                pngquant({ quality: [0.65, 0.8], speed: 4 }),
+                imagemin.svgo({ plugins: [{ removeViewBox: false }, { cleanupIDs: false }] }),
+                imagemin.gifsicle({ interlaced: true }),
+            ], { verbose: true, silent: false })
+        )
         .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
 });
 
+gulp.task("watch", function () {
+    gulp.watch("src/css/style.css", gulp.parallel("styles"));
+    gulp.watch("src/*.html", gulp.parallel("html"));
+    gulp.watch("src/js/**/*.js", gulp.parallel("scripts"));
+    gulp.watch("src/icons/**/*", gulp.parallel("icons"));
+    gulp.watch("src/img/**/*", gulp.parallel("images"));
+});
+
 gulp.task(
     "default",
-    gulp.parallel(
-        "watch",
-        "server",
-        "styles",
-        "scripts",
-        // "fonts",
-        "icons",
-        "html",
-        "images"
+    gulp.series(
+        gulp.parallel("styles", "scripts", "html", "icons", "images"),
+        gulp.parallel("watch", "server")
     )
 );
